74200e36e8d771a5a5c8dfb0cf4fb9f5fe3778d2
Saúl Laínez <saullainez@hotmail.es>
2021-05-10 09:12:58 -0600

    Credenciales para trabajar local y remotamente

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,13 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://firestore-grafica-42e5b-default-rtdb.firebaseio.com"
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
