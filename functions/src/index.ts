1da3ea36ac17d6e4dadddf815ec195194bee5828
Saúl Laínez <saullainez@hotmail.es>
2021-05-10 10:14:33 -0600

    POST
    Incrementar en 1 el voto del juego

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -37,11 +37,39 @@ const app = express();
 app.use(cors({origin:true}));
 
 app.get('/goty', async(req, res) => {
+
     const gotyRef = db.collection('goty');
     const docsSnap = await gotyRef.get();
     const games = docsSnap.docs.map( doc => doc.data() );
   
     res.json( games );
+
+});
+
+app.post('/goty/:id', async(req, res) => {
+    
+    const id = req.params.id;
+    const gameRef = db.collection('goty').doc( id );
+    const gameSnap = await gameRef.get();
+
+    if( !gameSnap.exists ){
+        res.status(404).json({
+            ok: false,
+            message: 'No existe un juego con el ID ' + id
+        })
+    }else{
+
+        const gameBefore = gameSnap.data() || { votes:0 };
+        await gameRef.update({
+            votes: gameBefore.votes + 1
+        });
+
+        res.json({
+            ok: true,
+            message: `Gracias por tu voto a ${ gameBefore.name }`
+        })
+    }
+
 });
 
 export const api = functions.https.onRequest( app );
