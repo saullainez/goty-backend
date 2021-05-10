713c223785bac1a8a1505c703228aa499a2b70aa
Saúl Laínez <saullainez@hotmail.es>
2021-05-10 10:04:30 -0600

    Servidor Express dentro de Firebase Cloud Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
 
 const serviceAccount = require("./serviceAccountKey.json");
 
@@ -29,3 +31,17 @@ export const getGOTY = functions.https.onRequest(async(request, response) => {
   response.json( games );
 
 });
+
+//Servidor de Express
+const app = express();
+app.use(cors({origin:true}));
+
+app.get('/goty', async(req, res) => {
+    const gotyRef = db.collection('goty');
+    const docsSnap = await gotyRef.get();
+    const games = docsSnap.docs.map( doc => doc.data() );
+  
+    res.json( games );
+});
+
+export const api = functions.https.onRequest( app );
