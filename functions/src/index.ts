f19337893dfdddec1f179861c408738972419d5a
Saúl Laínez <saullainez@hotmail.es>
2021-05-10 09:53:14 -0600

    Cloud Function: GET
    Obtener colecciones

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,10 +9,23 @@ admin.initializeApp({
     databaseURL: "https://firestore-grafica-42e5b-default-rtdb.firebaseio.com"
 });
 
+const db = admin.firestore();
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.json({message: "Hello from Firebase!!"});
+});
+
+export const getGOTY = functions.https.onRequest(async(request, response) => {
+  //const name = request.query.name || 'Sin nombre';
+  
+  const gotyRef = db.collection('goty');
+  const docsSnap = await gotyRef.get();
+  const games = docsSnap.docs.map( doc => doc.data() );
+
+  response.json( games );
+
 });
